a15ea2a8f9b2e37111a9ddaf3789b4dbaafb4536
Wtk0930 <734394415@qq.com>
2020-07-01 10:34:41 +0800

    dd

--- src/sys/ldk/com/view/LoginFrame.java
+++ src/sys/ldk/com/view/LoginFrame.java
@@ -52,7 +52,7 @@ public class LoginFrame extends JFrame {
 	 */
 	public LoginFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 562, 396);
+		setBounds(100, 100, 698, 379);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
